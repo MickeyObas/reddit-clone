9aba6f9115549b65f587c8c1c630e20d994d2c6c
Michael Goke <mikhzobby@gmail.com>
2025-02-28 19:45:15 +0100

    feat: home page added

--- frontend/src/components/FormInput.tsx
+++ frontend/src/components/FormInput.tsx
@@ -2,42 +2,27 @@ import exclamationIcon from '../assets/icons/exclamation-mark.png';
 import checkIcon from '../assets/icons/check.png';
 
 interface FormInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  type?: 'text' | 'email' | 'password',
-  placeholder: string,
   className?: string,
   containerClassName?: string,
   value?: string,
   error?: string,
   isValid: boolean,
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void,
-  onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void,
-  onFocus?: (e: React.FocusEvent<HTMLInputElement>) => void
 }
 
 export const FormInput: React.FC<FormInputProps> = ({
-  type='text',
-  placeholder,
   containerClassName,
   className,
   value,
   error,
   isValid,
-  onChange,
-  onBlur,
-  onFocus,
   ...rest
 }) => {
   return (
     <div className={`${containerClassName}`}>
       <div className="relative w-full">
         <input 
-          type={type}
-          placeholder={placeholder}
           className={`peer bg-gray-white p-4 border-0 outline-0 rounded-2xl w-full focus:outline-2 focus:outline-blue-500 ${error && 'outline-deep-red outline-2'} ${className}`}
           value={value}
-          onChange={onChange}
-          onBlur={onBlur}
-          onFocus={onFocus}
           {...rest}
           />
           {error ? (<img 
