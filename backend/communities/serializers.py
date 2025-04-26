8d359e2c1446a95acbf57d0f80711ea185975b5c
Michael Goke <mikhzobby@gmail.com>
2025-04-26 14:21:19 +0100

    lil bit more

--- backend/communities/serializers.py
+++ backend/communities/serializers.py
@@ -7,7 +7,7 @@ from topics.serializers import TopicCategorySerializer, TopicSerializer
 class CommunitySerializer(serializers.ModelSerializer):
     is_member = serializers.SerializerMethodField()
     moderators = UserSerializer(many=True, required=False)
-    topic_ids = serializers.ListField(write_only=True, child=serializers.IntegerField())
+    # topic_ids = serializers.ListField(write_only=True, child=serializers.IntegerField(), required=False)
     topics = TopicSerializer(many=True, read_only=True)
     
     class Meta:
@@ -27,7 +27,7 @@ class CommunitySerializer(serializers.ModelSerializer):
             'created_at',
             'is_member',
             'is_mature',
-            'topic_ids'
+            # 'topic_ids'
         ]
 
     def get_is_member(self, obj):
@@ -35,12 +35,12 @@ class CommunitySerializer(serializers.ModelSerializer):
         return obj.members.filter(id=user.id).exists()
     
     def create(self, validated_data):
-        topic_ids = validated_data.pop('topic_ids')
+        # topic_ids = validated_data.pop('topic_ids')
         request = self.context.get('request')
         user = request.user
 
         community = Community.objects.create(owner=user, **validated_data)
-        community.topics.set(topic_ids)
+        # community.topics.set(topic_ids)
         community.members.add(user.id)
         community.moderators.add(user.id)
 
