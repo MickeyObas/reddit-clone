1bce5a0f7953c51da33315a04b9c642202737187
Michael Goke <mikhzobby@gmail.com>
2025-05-05 22:47:14 +0100

    remove debug toolbar

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from debug_toolbar.toolbar import debug_toolbar_urls
+# from debug_toolbar.toolbar import debug_toolbar_urls
 
 
 urlpatterns = [
@@ -14,4 +14,4 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
-    urlpatterns += debug_toolbar_urls()
+    # urlpatterns += debug_toolbar_urls()
