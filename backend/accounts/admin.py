318304666dabbd199552dd67096181b515a0404a
Michael Goke <mikhzobby@gmail.com>
2025-05-13 11:29:55 +0100

    prep for guest creation

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,16 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from .models import User 
 
-from .models import User
-from profiles.models import Profile
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('username', 'email', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'groups')
+    search_fields = ('username', 'email') 
+    ordering = ('username',)
+    
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('email',)}),  # Replace these with your fields
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'custom_field_1', 'custom_field_2')  # Add custom fields
+        }),
+    )
 
 
-class UserModelAdmin(admin.ModelAdmin):
-    list_display = [
-        'email',
-        'username',
-        'created_at',
-        'is_active'
-    ]
+    form = UserChangeForm
+    add_form = UserCreationForm 
+    
+    filter_horizontal = ('groups', 'user_permissions')
 
-admin.site.register(User, UserModelAdmin)
-admin.site.register(Profile)
+admin.site.register(User, CustomUserAdmin)
